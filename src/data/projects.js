94bfc2f1746988ce1e8678f491a01e3881dbf39d
Mauricio Narvaez <mauricionarvilla@gmail.com>
2025-11-25 23:39:47 -0300

    fix, feat: agregar proyectos futuros,  arreglos de errores ortograficos, otras funciones

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     title: "Banco Nova",
     description:
-      "Una aplicación de banco fullstack con React, Ant Design y Tailwind CSS. Para el Backend Springboot, Docker, PostegreSQL.",
+      "Una aplicación de banco fullstack con React, Ant Design y Tailwind CSS. Para el Backend Springboot, Docker, PostgreSQL.",
     link: "https://github.com/madandev02/banco-nova",
     demo: "https://banco-nova.netlify.app/",
     image: "/images/banco-nova.png", // screenshot placeholder
@@ -14,6 +14,22 @@ const projects = [
     link: "https://github.com/madandev02/trading-lading",
     demo: "https://trading-lading-psi.vercel.app/",
     image: "/images/tradepro.png",
+  },
+  {
+    title: "NovaJobs Chile",
+    description:
+      "Una aplicación fulsltack de portal de empleo construida con el Stack MERN (MongoDB, Express.js, React, Node.js).",
+    link: "https://github.com/madandev02",
+    demo: "https://github.com/madandev02",
+    image: "/images/proximamente.jpg", // screenshot placeholder
+  },
+  {
+    title: "TradeNova",
+    description:
+      "Aplicación fullstack para trading (Stack a definir).",
+    link: "https://github.com/madandev02",
+    demo: "https://github.com/madandev02/",
+    image: "/images/proximamente.jpg", // screenshot placeholder
   },
 ];
 
